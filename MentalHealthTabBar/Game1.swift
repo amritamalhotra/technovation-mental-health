4cc9f8bddc0b4c4c3d8bddd9c0e9d8cb13e73dbf
amritamalhotra <amrita.malhotra@gmail.com>
2020-03-29 12:21:22 -0700

    3/29/2020 recommended music based off of emotion is working!

--- MentalHealthTabBar/Game1.swift
+++ MentalHealthTabBar/Game1.swift
@@ -11,8 +11,31 @@ import AVFoundation
 
 class Game1: UIViewController {
     var audioPlayer = AVAudioPlayer()
+    
+    @IBOutlet weak var displayedImage: UIImageView!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button10: UIButton!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        button2.alpha = 0
+        button3.alpha = 0
+        button4.alpha = 0
+        button5.alpha = 0
+        button6.alpha = 0
+        button7.alpha = 0
+        button8.alpha = 0
+        button9.alpha = 0
+        button10.alpha = 0
         
         let sound = Bundle.main.path(forResource: "SampleAudio_0.4mb", ofType: "mp3")
         
@@ -37,14 +60,44 @@ class Game1: UIViewController {
         audioPlayer.stop()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func button1Hit(_ sender: Any) {
+        button2.alpha = 1
+        button1.alpha = 0
     }
-    */
-
+    @IBAction func button2Hit(_ sender: Any) {
+        button3.alpha = 1
+        button2.alpha = 0
+    }
+    @IBAction func button3Hit(_ sender: Any) {
+        button4.alpha = 1
+        button3.alpha = 0
+    }
+    @IBAction func button4Hit(_ sender: Any) {
+        button5.alpha = 1
+        button4.alpha = 0
+    }
+    @IBAction func button5Hit(_ sender: Any) {
+        button6.alpha = 1
+        button5.alpha = 0
+    }
+    @IBAction func button6Hit(_ sender: Any) {
+        button7.alpha = 1
+        button6.alpha = 0
+    }
+    @IBAction func button7Hit(_ sender: Any) {
+        button8.alpha = 1
+        button7.alpha = 0
+    }
+    @IBAction func button8Hit(_ sender: Any) {
+        button9.alpha = 1
+        button8.alpha = 0
+    }
+    @IBAction func button9Hit(_ sender: Any) {
+        button10.alpha = 1
+        button9.alpha = 0
+    }
+    @IBAction func button10Hit(_ sender: Any) {
+        print("Completed")
+    }
+    
 }
--- MentalHealthTabBar/Music.swift
+++ MentalHealthTabBar/Music.swift
@@ -7,27 +7,169 @@
 //
 
 import UIKit
+import AVFoundation
 
 class Music: UIViewController {
+    var h1Player = AVAudioPlayer()
+    var h2Player = AVAudioPlayer()
+    var sad1Player = AVAudioPlayer()
+    var sad2Player = AVAudioPlayer()
+    var str1Player = AVAudioPlayer()
+    var str2Player = AVAudioPlayer()
+    var bor1Player = AVAudioPlayer()
+    var bor2Player = AVAudioPlayer()
+    var mad1Player = AVAudioPlayer()
+    var mad2Player = AVAudioPlayer()
+    var anx1Player = AVAudioPlayer()
+    var anx2Player = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let h1sound =  Bundle.main.path(forResource: "HappyOne", ofType: "mp3")
+        
+        do {
+            h1Player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: h1sound!))
+            }
+        catch {
+            print(error)
+        }
+        let h2sound =  Bundle.main.path(forResource: "HappyTwo", ofType: "mp3")
+        
+        do {
+            h2Player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: h2sound!))
+            }
+        catch {
+            print(error)
+        }
+        let sad1sound =  Bundle.main.path(forResource: "SadOne", ofType: "mp3")
+        
+        do {
+            sad1Player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sad1sound!))
+            }
+        catch {
+            print(error)
+        }
+        let sad2sound =  Bundle.main.path(forResource: "SadTwo", ofType: "mp3")
+        
+        do {
+            sad2Player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sad2sound!))
+            }
+        catch {
+            print(error)
+        }
+        let str1sound =  Bundle.main.path(forResource: "StressedOne", ofType: "mp3")
+        
+        do {
+            str1Player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: str1sound!))
+            }
+        catch {
+            print(error)
+        }
+        let str2sound =  Bundle.main.path(forResource: "StressedTwo", ofType: "mp3")
+        
+        do {
+            str2Player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: str2sound!))
+            }
+        catch {
+            print(error)
+        }
+        let bor1sound =  Bundle.main.path(forResource: "BoredOne", ofType: "mp3")
+        
+        do {
+            bor1Player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: bor1sound!))
+            }
+        catch {
+            print(error)
+        }
+        let bor2sound =  Bundle.main.path(forResource: "BoredTwo", ofType: "mp3")
+        
+        do {
+            bor2Player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: bor2sound!))
+            }
+        catch {
+            print(error)
+        }
+        let mad1sound =  Bundle.main.path(forResource: "MadOne", ofType: "mp3")
+        
+        do {
+            mad1Player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: mad1sound!))
+            }
+        catch {
+            print(error)
+        }
+        let mad2sound =  Bundle.main.path(forResource: "MadTwo", ofType: "mp3")
+        
+        do {
+            mad2Player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: mad2sound!))
+            }
+        catch {
+            print(error)
+        }
+        let anx1sound =  Bundle.main.path(forResource: "AnxiousOne", ofType: "mp3")
+        
+        do {
+            anx1Player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: anx1sound!))
+            }
+        catch {
+            print(error)
+        }
+        let anx2sound =  Bundle.main.path(forResource: "AnxiousTwo", ofType: "mp3")
+        
+        do {
+            anx2Player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: anx2sound!))
+            }
+        catch {
+            print(error)
+        }
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func music1Play(_ sender: Any) {
+        let currentEmotion = Emotions.shared.theEmotion
+        if (currentEmotion == "happy!") {
+            h1Player.play()
+        }
+        else if (currentEmotion == "sad") {
+            sad1Player.play()
+        }
+        if (currentEmotion == "stressed") {
+            str1Player.play()
+        }
+        if (currentEmotion == "bored") {
+            bor1Player.play()
+        }
+        if (currentEmotion == "mad") {
+            mad1Player.play()
+        }
+        if (currentEmotion == "anxious") {
+            anx1Player.play()
+        }
+    }
+    @IBAction func music2Play(_ sender: Any) {
+        let currentEmotion = Emotions.shared.theEmotion
+        if (currentEmotion == "happy!") {
+            h2Player.play()
+        }
+        if (currentEmotion == "sad") {
+            sad2Player.play()
+        }
+        if (currentEmotion == "stressed") {
+            str2Player.play()
+        }
+        if (currentEmotion == "bored") {
+            bor2Player.play()
+        }
+        if (currentEmotion == "mad") {
+            mad2Player.play()
+        }
+        if (currentEmotion == "anxious") {
+            anx2Player.play()
+        }
+    }
     
     @IBAction func toMusicMaker(_ sender: UIButton) {
         performSegue(withIdentifier: "toMakeMusic", sender: self)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
